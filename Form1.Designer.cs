a16ec42513401a64af2c112bcb752db367151805
Alperen10 <alperenakgun1905@outlook.com>
2020-10-22 15:33:19 +0300

    telefon defteri

---
+++ Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace PhoneBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxNumber = new System.Windows.Forms.TextBox();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.tbxEmail = new System.Windows.Forms.TextBox();
+            this.tbxAddress = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxSearch = new System.Windows.Forms.TextBox();
+            this.telefonDefteriBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.appData = new PhoneBook.AppData();
+            this.telefonNumarasıDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adSoyadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adresDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ıDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.telefonDefteriBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(911, 547);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(87, 36);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Kaydet";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.telefonNumarasıDataGridViewTextBoxColumn,
+            this.adSoyadDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn,
+            this.adresDataGridViewTextBoxColumn,
+            this.ıDDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.telefonDefteriBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 353);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(973, 188);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbxAddress);
+            this.panel1.Controls.Add(this.tbxEmail);
+            this.panel1.Controls.Add(this.tbxName);
+            this.panel1.Controls.Add(this.tbxNumber);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(985, 283);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Telefon Numarası:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad-Soyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Adres:";
+            // 
+            // tbxNumber
+            // 
+            this.tbxNumber.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.telefonDefteriBindingSource, "Telefon Numarası", true));
+            this.tbxNumber.Location = new System.Drawing.Point(152, 38);
+            this.tbxNumber.Name = "tbxNumber";
+            this.tbxNumber.Size = new System.Drawing.Size(217, 22);
+            this.tbxNumber.TabIndex = 4;
+            // 
+            // tbxName
+            // 
+            this.tbxName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.telefonDefteriBindingSource, "Ad-Soyad", true));
+            this.tbxName.Location = new System.Drawing.Point(152, 84);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(360, 22);
+            this.tbxName.TabIndex = 5;
+            // 
+            // tbxEmail
+            // 
+            this.tbxEmail.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.telefonDefteriBindingSource, "Email", true));
+            this.tbxEmail.Location = new System.Drawing.Point(152, 140);
+            this.tbxEmail.Name = "tbxEmail";
+            this.tbxEmail.Size = new System.Drawing.Size(621, 22);
+            this.tbxEmail.TabIndex = 6;
+            // 
+            // tbxAddress
+            // 
+            this.tbxAddress.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.telefonDefteriBindingSource, "Adres", true));
+            this.tbxAddress.Location = new System.Drawing.Point(152, 195);
+            this.tbxAddress.Multiline = true;
+            this.tbxAddress.Name = "tbxAddress";
+            this.tbxAddress.Size = new System.Drawing.Size(817, 72);
+            this.tbxAddress.TabIndex = 7;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(818, 547);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(87, 36);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "İptal";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(725, 547);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(87, 36);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Düzenle";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(632, 547);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(87, 36);
+            this.btnNew.TabIndex = 5;
+            this.btnNew.Text = "Yeni";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Arama:";
+            // 
+            // tbxSearch
+            // 
+            this.tbxSearch.Location = new System.Drawing.Point(165, 303);
+            this.tbxSearch.Multiline = true;
+            this.tbxSearch.Name = "tbxSearch";
+            this.tbxSearch.Size = new System.Drawing.Size(833, 34);
+            this.tbxSearch.TabIndex = 7;
+            this.tbxSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxSearch_KeyPress);
+            // 
+            // telefonDefteriBindingSource
+            // 
+            this.telefonDefteriBindingSource.DataMember = "TelefonDefteri";
+            this.telefonDefteriBindingSource.DataSource = this.appData;
+            // 
+            // appData
+            // 
+            this.appData.DataSetName = "AppData";
+            this.appData.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // telefonNumarasıDataGridViewTextBoxColumn
+            // 
+            this.telefonNumarasıDataGridViewTextBoxColumn.DataPropertyName = "Telefon Numarası";
+            this.telefonNumarasıDataGridViewTextBoxColumn.HeaderText = "Telefon Numarası";
+            this.telefonNumarasıDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.telefonNumarasıDataGridViewTextBoxColumn.Name = "telefonNumarasıDataGridViewTextBoxColumn";
+            this.telefonNumarasıDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // adSoyadDataGridViewTextBoxColumn
+            // 
+            this.adSoyadDataGridViewTextBoxColumn.DataPropertyName = "Ad-Soyad";
+            this.adSoyadDataGridViewTextBoxColumn.HeaderText = "Ad-Soyad";
+            this.adSoyadDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.adSoyadDataGridViewTextBoxColumn.Name = "adSoyadDataGridViewTextBoxColumn";
+            this.adSoyadDataGridViewTextBoxColumn.Width = 240;
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "Email";
+            this.emailDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            this.emailDataGridViewTextBoxColumn.Width = 240;
+            // 
+            // adresDataGridViewTextBoxColumn
+            // 
+            this.adresDataGridViewTextBoxColumn.DataPropertyName = "Adres";
+            this.adresDataGridViewTextBoxColumn.HeaderText = "Adres";
+            this.adresDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.adresDataGridViewTextBoxColumn.Name = "adresDataGridViewTextBoxColumn";
+            this.adresDataGridViewTextBoxColumn.Width = 240;
+            // 
+            // ıDDataGridViewTextBoxColumn
+            // 
+            this.ıDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.ıDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.ıDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.ıDDataGridViewTextBoxColumn.Name = "ıDDataGridViewTextBoxColumn";
+            this.ıDDataGridViewTextBoxColumn.Visible = false;
+            this.ıDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1004, 585);
+            this.Controls.Add(this.tbxSearch);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Telefon Defteri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.telefonDefteriBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tbxAddress;
+        private System.Windows.Forms.TextBox tbxEmail;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.TextBox tbxNumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxSearch;
+        private System.Windows.Forms.BindingSource telefonDefteriBindingSource;
+        private AppData appData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefonNumarasıDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adSoyadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adresDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ıDDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        static AppData db;
+        protected static AppData App
+        {
+            get
+            {
+                if (db==null)
+                {
+                    db = new AppData();
+
+                }
+                return db;
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string fileName = string.Format("{0}//data.dat",Application.StartupPath);
+            if (File.Exists(fileName))
+            {
+                App.TelefonDefteri.ReadXml(fileName);
+                telefonDefteriBindingSource.DataSource = App.TelefonDefteri;
+                panel1.Enabled = false;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                telefonDefteriBindingSource.EndEdit();
+                App.TelefonDefteri.AcceptChanges();
+                App.TelefonDefteri.WriteXml(string.Format("{0}//data.dat", Application.StartupPath));
+                panel1.Enabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                App.TelefonDefteri.RejectChanges();
+            }
+
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = true;
+            tbxNumber.Focus();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            telefonDefteriBindingSource.ResetBindings(false);
+            panel1.Enabled = false;
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                panel1.Enabled = true;
+                App.TelefonDefteri.AddTelefonDefteriRow(App.TelefonDefteri.NewTelefonDefteriRow());
+                telefonDefteriBindingSource.MoveLast();
+                tbxNumber.Focus();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                App.TelefonDefteri.RejectChanges();
+            }
+           
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (MessageBox.Show("Bu kaydı silmek istediğinize emin misiniz?","Message",MessageBoxButtons.YesNo,MessageBoxIcon.Question)==DialogResult.Yes)
+
+                {
+                    telefonDefteriBindingSource.RemoveCurrent();
+
+                }
+            }
+
+        }
+
+        private void tbxSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+
+
+                if (!string.IsNullOrEmpty(tbxSearch.Text))
+                {
+                    var query = from o in App.TelefonDefteri
+                                where o.Telefon_Numarası == tbxSearch.Text || o._Ad_Soyad.Contains(tbxSearch.Text)
+                                || o.Email == tbxSearch.Text
+                                select o;
+                    dataGridView1.DataSource = query.ToList();
+                }
+                else
+                {
+                    dataGridView1.DataSource = telefonDefteriBindingSource;
+                }
+            }
+    
+
+
+        }
+    }
+}
